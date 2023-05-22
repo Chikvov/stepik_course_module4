b68d8efef02be4023fafec5d82619825bc44cdbf
My Name <my@email.com>
2023-05-22 20:08:23 +0300

    lesson 4.3 step 3 done

--- test_product_page.py
+++ test_product_page.py
@@ -2,7 +2,7 @@ from .pages.product_page import ProductPage
 import time
 
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
 
 
 def test_guest_can_add_product_to_basket(browser):
@@ -10,6 +10,6 @@ def test_guest_can_add_product_to_basket(browser):
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
-    # time.sleep(100)
+    time.sleep(10)
     page.add_message_is_correct()
     page.basket_price_message_is_correct()
